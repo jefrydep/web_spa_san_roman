2f59512be80382170f8fadcaafd364ca92ac78db
jefry <jefrydep@gmail.com>
2023-05-24 14:54:01 -0500

    working about us direcory

--- components/asesores/CardAsesores.tsx
+++ components/asesores/CardAsesores.tsx
@@ -1,1 +1,71 @@
-import React, { ReactNode } from 'react'
+import Image from 'next/image'
+import React from 'react'
+import { MdCall, MdEmail, MdFacebook, MdWhatsapp } from 'react-icons/md'
+
+const CardAsesores = () => {
+    return (
+        < >
+            <div className='shadow-lg py-8 bg-white rounded-xl px-5'>
+
+                <div className='  flex items-center flex-col gap-2 mb-5'>
+                    <div className='rounded-full shadow-xl w-44 h-44   bg-cover ' style={{ backgroundImage: `url("/assets/img/eliot.png")` }}>
+
+                        {/* <Image className=' ' height={ 0} width={0 } alt='Jefrydep' src="/assets/img/mr-robot.jpg" /> */}
+                    </div>
+
+                    <h3>Palomino Mayta Godofredo</h3>
+                    <h4>power dev</h4>
+                    <div className='flex gap-4'>
+                        <div className='bg-green-500 rounded-full w-10 h-10 flex items-center justify-center'> <MdWhatsapp size={24}   color='white'  /></div>
+                        <div className='bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center'><MdEmail size={24}   color='white' /></div>
+                        <div className='bg-red-500 rounded-full w-10 h-10 flex items-center justify-center'> <MdCall  size={24}  color='white'/></div>
+                    </div>
+                </div>
+                <div>
+                    <div className='flex'>
+                        <h5 className="text-xl font-bold text-primary-sm0 mx-2">
+                            <MdWhatsapp  />
+                        </h5>
+                        <a
+                            className="text-lg text-second-sm "
+                            target="_blank"
+                            href="https://wa.me/51953306637"
+                            
+                        >
+                            953306637
+                        </a>
+                    </div>
+                    <div className='flex'>
+                        <h5 className="text-xl font-bold text-primary-sm0 mx-2">
+                            <MdEmail />
+                        </h5>
+                        <a
+                            className="text-lg text-second-sm"
+                            target="_blank"
+                            href="https://www.gmail.com/mail/help/intl/es/about.html?iframe"
+                        >
+                            inmobiliariasanro@gmail.com
+                        </a>
+                    </div>
+                    <div className='flex'>
+                        <h5 className="text-xl font-bold text-primary-sm0 mx-2">
+                            <MdFacebook />
+                        </h5>
+                        <a
+                            className="text-lg text-second-sm"
+                            target="_blank"
+                            href="https://www.facebook.com/SanRomanConstructoraInmobiliaria/"
+                        >
+                            facebook
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </ >
+    )
+}
+
+export default CardAsesores
--- pages/asesores/index.tsx
+++ pages/asesores/index.tsx
@@ -1,28 +1,49 @@
-import Hero from '@/components/hero/Hero';
-import CardNosotros from '@/components/nosotros/CardNosotros';
-import Image from 'next/image';
-import React from 'react'
-export default function asesores() {
-  const dataHome = [
-    {
-      slogan:
-        "Nuestros asesores",
-      imgUrl: "/assets/img/nosotros.jpg",
-    },
-  ];
-  return (
-    <>
-      <Hero imgUrl={dataHome[0].imgUrl} slogan={dataHome[0].slogan} />
+import CardAsesores from "@/components/asesores/CardAsesores";
 
-      <div className='container m-auto  '>
-        <div className='bg-white  flex  items-center flex-col py-6'>
-          <section className='px-4 mx-2 sm:w-2/3 items-center flex flex-col sm:grid sm:grid-cols-2 mb-2 pb-2 pt-3 mt-2 bg-white'>
-            <div className=''>
-              {/* <Image src='/assets/img/nosotros.jpg' height={500} width={400} alt='San Roman' /> */}
-            </div>
-            </section>
-            </div>
-            </div>
-             
-            </>)
-             }
+export default function asesores() {
+
+
+
+
+  return (
+
+    <div className="bg-white ">
+
+      <div className="bg-second-sm py-5 mb-20">
+
+        <h2 className="text-6xl text-primary-sm text-center">Nuestros Asesores</h2>
+      </div>
+
+      <section className="container m-auto">
+        <section className="grid grid-cols-4 gap-4" >
+
+
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+          <CardAsesores />
+
+        </section>
+
+      </section>
+    </div>
+
+
+
+  )
+
+
+
+
+
+
+
+}
